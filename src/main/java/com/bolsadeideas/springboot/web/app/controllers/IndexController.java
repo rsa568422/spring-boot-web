b30efad7e1e7f577eb7149f76a836ed18e0d5adf
Roberto <Roberto@DESKTOP-TNV931M.technicolor.net>
2021-09-18 22:10:26 +0200

    Clase 24:
    Uso de application.properties para los textos de la aplicación.

--- src/main/java/com/bolsadeideas/springboot/web/app/controllers/IndexController.java
+++ src/main/java/com/bolsadeideas/springboot/web/app/controllers/IndexController.java
@@ -3,6 +3,7 @@ package com.bolsadeideas.springboot.web.app.controllers;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,10 +15,19 @@ import com.bolsadeideas.springboot.web.app.models.Usuario;
 @Controller
 @RequestMapping("/app")
 public class IndexController {
+	
+	@Value("${texto.indexcontroller.index.titulo}")
+	private String textoIndex;
+	
+	@Value("${texto.indexcontroller.perfil.titulo}")
+	private String textoPerfil;
+	
+	@Value("${texto.indexcontroller.listar.titulo}")
+	private String textoListar;
 
 	@GetMapping({ "/index", "/", "", "/home" })
 	public String index(Model model) {
-		model.addAttribute("titulo", "Hola Spring con Model!");
+		model.addAttribute("titulo", textoIndex);
 
 		return "index";
 	}
@@ -31,14 +41,14 @@ public class IndexController {
 		usuario.setEmail("roberto@correo.com");
 
 		model.addAttribute("usuario", usuario);
-		model.addAttribute("titulo", "Perfil del usuario: ".concat(usuario.getNombre()));
+		model.addAttribute("titulo", textoPerfil.concat(usuario.getNombre()));
 
 		return "perfil";
 	}
 
 	@GetMapping("/listar")
 	public String listar(Model model) {
-		model.addAttribute("titulo", "Listado de usuarios");
+		model.addAttribute("titulo", textoListar);
 
 		return "listar";
 	}
