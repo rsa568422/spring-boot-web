4772dad1c04671f19365e9829c6bef013f963468
Roberto <Roberto@DESKTOP-TNV931M.technicolor.net>
2021-09-18 11:37:47 +0200

    Se agrega la vista listar.html junto al método con el que acceder a ella en el controlador IndexController.
    Se agrega email a la clase Usuario y se hace uso de el en el perfil para
    mostrarlo si no es null.

--- src/main/java/com/bolsadeideas/springboot/web/app/models/Usuario.java
+++ src/main/java/com/bolsadeideas/springboot/web/app/models/Usuario.java
@@ -5,6 +5,8 @@ public class Usuario {
 	private String nombre;
 
 	private String apellido;
+	
+	private String email;
 
 	public String getNombre() {
 		return nombre;
@@ -20,6 +22,14 @@ public class Usuario {
 
 	public void setApellido(String apellido) {
 		this.apellido = apellido;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
 	}
 
 }
