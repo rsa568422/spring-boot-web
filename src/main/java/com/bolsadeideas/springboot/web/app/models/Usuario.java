07472c099432d5a1f11d9fb894ea9b41e6680e03
Roberto <Roberto@DESKTOP-TNV931M.technicolor.net>
2021-09-18 12:05:05 +0200

    Clase 17:
    Se agregan constructores a la clase Usuario.
    
    Se genera una lista de usuarios en el controlador para la vista
    listar.html.
    
    Se muestra una tabla con los usuarios con th:each.

--- src/main/java/com/bolsadeideas/springboot/web/app/models/Usuario.java
+++ src/main/java/com/bolsadeideas/springboot/web/app/models/Usuario.java
@@ -5,8 +5,18 @@ public class Usuario {
 	private String nombre;
 
 	private String apellido;
-	
+
 	private String email;
+
+	public Usuario() {
+
+	}
+
+	public Usuario(String nombre, String apellido, String email) {
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.email = email;
+	}
 
 	public String getNombre() {
 		return nombre;
